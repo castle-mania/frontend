ed2ee3bbe767b6f60385b5985f1699124bd1348c
Donnie <donnieacolston@gmail.com>
2022-01-02 02:43:55 +0000

    redux learning

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import React from 'react';
 import './App.css';
 import {ChakraProvider} from '@chakra-ui/react';
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from '@reduxjs/toolkit';
 import NavBar from './components/navbar';
 import DiscordAuth from './auth';
 import Home from './pages/home';
+import {userSlice} from './stores/user';
+
+const store = createStore(
+  userSlice.reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 function App() {
   return (
     <ChakraProvider>
-      <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/discord/callback" exact element={<DiscordAuth />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <NavBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/discord/callback" exact element={<DiscordAuth />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </ChakraProvider>
   );
 }
