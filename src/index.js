337c1758c3520f42fb78bffbaa6ec1bf634003b5
dclstn <donnieacolston@gmail.com>
2022-04-13 23:41:29 +0100

    sentry fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ Sentry.init({
   tracesSampleRate: 1.0,
   beforeSend(event, hint) {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-      console.error(hint.originalException || hint.syntheticException);
+      console.error(hint.originalException ?? hint.syntheticException);
       return null;
     }
 
